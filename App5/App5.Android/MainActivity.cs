3141010b2c47b9475016c44f223bb2e163e2b1c8
Milan Laslop <milan33@gmail.com>
2017-05-15 08:51:58 +0200

    initial

---
+++ App5/App5.Android/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App5.Droid
+{
+    [Activity(Label = "App5", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    // [Activity(Label = "App5", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    // public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ App5/App5/App.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Xamarin.Forms;
+
+namespace App5
+{
+    public partial class App : Application
+    {
+
+        private TabbedPage tp;
+
+        private int count_button;
+        private int count_list;
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(CreateTabbedPage());
+        }
+
+        private Page CreateTabbedPage()
+        {
+            tp = new TabbedPage();
+            {
+                ContentPage page = new ContentPage();
+                page.Title = "Page 1";
+
+                StackLayout sl = new StackLayout();
+                sl.Children.Add(new Label() { Text = "This is page 1" });
+
+                Label counter_button = new Label();
+                sl.Children.Add(counter_button);
+
+                Label counter_list = new Label();
+                sl.Children.Add(counter_list);
+
+                {
+                    Button button = new Button() { Text = "Button" };
+                    button.Clicked += (sender, e) =>
+                    {
+                        ++count_button;
+                        counter_button.Text = "Button clicked: " + count_button;
+                    };
+                    sl.Children.Add(button);
+                }
+
+                {
+                    ListView list = new ListView();
+                    list.HasUnevenRows = true;
+                    list.ItemsSource = new string[] { "List item" };
+                    list.ItemTapped += (sender, e) =>
+                    {
+                        ++count_list;
+                        counter_list.Text = "List clicked: " + count_list;
+                    };
+                    sl.Children.Add(list);
+                }
+
+                page.Content = sl;
+                tp.Children.Add(page);
+            }
+
+            {
+                ContentPage page = new ContentPage();
+                page.Title = "Page 2";
+
+                StackLayout sl = new StackLayout();
+                sl.Children.Add(new Label() { Text = "This is page 2" });
+
+                page.Content = sl;
+                tp.Children.Add(page);
+            }
+
+            return tp;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
